5cd4c5a4d79588a8b46e44ca9787c2aa2d3abebb
Enoch Finley <enochfinley@gmail.com>
2019-10-13 12:48:02 -0500

    add destroyable to Turret

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -9,8 +9,10 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public float spawnDelay = 10f;
+    public float lineDisplayTime = .5f;
 
-    public float nextSpawnTime;
+    float nextSpawnTime;
+    float removeLineRenderTime;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +20,15 @@ public class Gun : MonoBehaviour
         if (Input.GetKey(KeyCode.Mouse0))
         {
             Shoot();
+        }
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            DestroyObj();
+        }
+        if (LineRender.enabled && Time.time > removeLineRenderTime)
+        {
+            Debug.Log("Removed LineRender");
+            LineRender.enabled = false;
         }
     }
 
@@ -29,11 +40,14 @@ public class Gun : MonoBehaviour
 
         if (didHit)
         {
-            LineRender.SetPositions(new Vector3[] { transform.position, hit.point});
+            Debug.Log($"Ending Display of line at: {removeLineRenderTime}");
 
             if (CanSpawn())
             {
                 Spawn(hit.point);
+                LineRender.SetPositions(new Vector3[] { transform.position, hit.point });
+                removeLineRenderTime = Time.time + lineDisplayTime;
+                LineRender.enabled = true;
             }
             Debug.Log(hit.transform.name);
         }
@@ -41,6 +55,27 @@ public class Gun : MonoBehaviour
         {
             Debug.Log("miss!");
         }
+    }
+
+    void DestroyObj()
+    {
+        RaycastHit hit;
+
+        var didHit = Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range);
+
+        if (didHit)
+        {
+            if (hit.collider.CompareTag("Destroyable"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+            Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            Debug.Log("miss!");
+        }
+
     }
 
     private void Spawn(Vector3 spawnLoc)
